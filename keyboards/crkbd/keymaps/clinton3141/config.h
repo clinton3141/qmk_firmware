478989ca6755470dd7118756f0651f820b6b834b
Clinton Montague <clinton3141@gmail.com>
2025-02-01 09:21:01 +0000

    OLED brightness

--- keyboards/crkbd/keymaps/clinton3141/config.h
+++ keyboards/crkbd/keymaps/clinton3141/config.h
@@ -20,6 +20,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
+#ifdef OLED_ENABLE
+    #define OLED_BRIGHTNESS 1
+#endif
+
 //#define USE_MATRIX_I2C
 
 //#define QUICK_TAP_TERM 0
