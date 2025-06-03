3962db2c485944ed506588184c4356b226987f2c
Clinton Montague <clinton3141@gmail.com>
2025-06-03 21:49:10 +0100

    Home row mods

--- keyboards/crkbd/keymaps/clinton3141/keymap.c
+++ keyboards/crkbd/keymaps/clinton3141/keymap.c
@@ -18,16 +18,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include QMK_KEYBOARD_H
 
-enum custom_keycodes {
-    CM_ARR = SAFE_RANGE,
-    CM_SHFT_MOUSE,
-};
-
 enum layers {
     _BASE,
     _NUMS,
     _MODS_NAV,
-    _MOUSE,
 };
 
 #define MOD_ACTIVE_COLOUR 0x16, 0x16, 0x00
@@ -35,13 +29,13 @@ enum layers {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-       KC_ESC,    KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,                         KC_J,    KC_L,    KC_U,    KC_Y,KC_SCLN,  KC_BSPC,
-  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-       KC_TAB,    KC_A,    KC_R,    KC_S,    KC_T,    KC_G,                         KC_M,    KC_N,    KC_E,    KC_I,   KC_O,  KC_QUOT,
+       KC_ESC,    KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,                         KC_J,    KC_L,    KC_U,    KC_Y,KC_SCLN,  KC_BSLS,
+//|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+         KC_TAB, LSFT_T(KC_A), LCTL_T(KC_R), LALT_T(KC_S), LGUI_T(KC_T),    KC_G,                         KC_M, RGUI_T(KC_N), RALT_T(KC_E), RCTL_T(KC_I), RSFT_T(KC_O),  KC_QUOT,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
      KC_GRAVE,    KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,                         KC_K,    KC_H, KC_COMM,  KC_DOT, KC_SLSH,  KC_BSLS,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                        KC_LCTL, KC_SPC, MO(_MODS_NAV),     MO(_NUMS), CM_SHFT_MOUSE, KC_ENT
+                                        KC_BACKSPACE, KC_SPC, MO(_MODS_NAV),     MO(_NUMS), KC_LSFT, KC_ENT
                                       //`--------------------------'  `--------------------------'
 
   ),
@@ -52,7 +46,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       _______,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,   KC_GT,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      _______, KC_UNDS, KC_MINS, KC_PLUS,  KC_EQL,  CM_ARR,                      XXXXXXX, KC_LCBR, KC_RCBR, KC_LBRC, KC_RBRC,   KC_LT,
+      _______, KC_UNDS, KC_MINS, KC_PLUS,  KC_EQL,  XXXXXXX,                      XXXXXXX, KC_LCBR, KC_RCBR, KC_LBRC, KC_RBRC,   KC_LT,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           _______, _______, _______,     _______,   _______, _______
                                       //`--------------------------'  `--------------------------'
@@ -68,17 +62,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           _______, _______, _______,     _______,   _______, _______
                                       //`--------------------------'  `--------------------------'
-  ),
-    [_MOUSE] = LAYOUT_split_3x6_3(
-  //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-      _______, XXXXXXX, XXXXXXX,   MS_UP, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      XXXXXXX, XXXXXXX, MS_LEFT, MS_DOWN, MS_RGHT, MS_BTN1,                      XXXXXXX, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, XXXXXXX,
-  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, MS_BTN2,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-  //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          _______, _______, _______,     _______,   _______, _______
-                                      //`--------------------------'  `--------------------------'
   )
 };
 
@@ -89,51 +72,12 @@ bool is_alt_held(void) { return (get_oneshot_mods() | get_mods()) & MOD_MASK_ALT
 bool is_gui_held(void) { return (get_oneshot_mods() | get_mods()) & MOD_MASK_GUI; }
 bool is_shift_held(void) { return (get_oneshot_mods() | get_mods()) & MOD_MASK_SHIFT; }
 
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    static uint16_t last_tap_time = 0;
-    static bool is_shift_held = false;
-
-    switch (keycode) {
-        case CM_ARR:
-            if (record->event.pressed) {
-                SEND_STRING("->");
-            }
-            break;
-        case CM_SHFT_MOUSE:
-            if (record->event.pressed) {
-                if (timer_elapsed(last_tap_time) < TAPPING_TERM) {
-                    layer_on(_MOUSE);
-                } else {
-                    register_code(KC_LSFT);
-                    is_shift_held = true;
-                }
-                last_tap_time = timer_read();
-            } else {
-                if (is_shift_held) {
-                    unregister_code(KC_LSFT);
-                    is_shift_held = false;
-                } else {
-                    layer_off(_MOUSE);
-                }
-            }
-            return false;
-    }
-    return true;
-}
 
 bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
-    if (layer_state_is(_MOUSE)) {
-        rgb_matrix_set_color(11, RGB_CYAN);
-        rgb_matrix_set_color(16, RGB_CYAN);
-        rgb_matrix_set_color(17, RGB_CYAN);
-        rgb_matrix_set_color(19, RGB_CYAN);
-        return false;
-    } else {
         rgb_matrix_set_color(11, RGB_BLACK);
         rgb_matrix_set_color(16, RGB_BLACK);
         rgb_matrix_set_color(17, RGB_BLACK);
         rgb_matrix_set_color(19, RGB_BLACK);
-    }
     if (is_gui_held()) {
         rgb_matrix_set_color(11, RGB_WHITE);
     } else {
@@ -168,8 +112,7 @@ bool oled_task_user(void) {
     bool alt_active = (mods | oneshot) & MOD_MASK_ALT;
     bool ctrl_active = (mods | oneshot) & MOD_MASK_CTRL;
     bool shift_active = (mods | oneshot) & MOD_MASK_SHIFT;
-    bool mouse_active = layer_state_is(_MOUSE);
-    bool any_active = gui_active || alt_active || ctrl_active || shift_active || mouse_active;
+    bool any_active = gui_active || alt_active || ctrl_active || shift_active;
 
     if (!any_active) {
         oled_off();
@@ -186,10 +129,6 @@ bool oled_task_user(void) {
     oled_write_P(PSTR("         CMD     \n"), gui_active);
     oled_write_P(PSTR("             SFT \n"), shift_active);
 
-    // TODO: this overwrites CTL. Ideally it should go somewhere else.
-    if (layer_state_is(_MOUSE)) {
-        oled_write_P(PSTR("MOUSE\n"), false);
-    }
     return false;
 }
 #endif
